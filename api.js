e72f760c8748f6ab12e9b0ac99a7672365eed8a7
Courtney Cline <courtalex134@gmail.com>
2023-06-26 19:45:22 -0400

    thrid push

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ async function fetchData() {
       });
       const data = await response.json();
       console.log(data)
-      return data; // Return the data as a Promise
+      return data.articles; // Return the data as a Promise
     } catch (error) {
       console.log('Error:', error);
     }
@@ -17,12 +17,14 @@ async function fetchData() {
     const outputContainer = document.getElementById('info');
   
     // Extract properties from the data object
-    const news1 = articles;
-    const news2 = articles;  
+    const news1 = data.articles[0];
+    const news2 = data.articles[1];  
     // Create HTML markup
     const markup = `
-      <h2>News Story: ${news1}</h2>
-      <h2>News Story: ${news2}</h2>
+      <h2>News Story: ${articles[0].title}</h2>
+      <p>Description: ${article[1].description}</p>
+      <h2>News Story: ${articles[1].title}</h2>
+      <p>Description: ${articles[2].description}</p>
     `;
   
     // Update the HTML container with the generated markup
